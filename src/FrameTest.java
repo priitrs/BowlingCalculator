2a81d8b49cb2efa30e21abf3d7a10cfcf3210543
PriitR <priitrs@gmail.com>
2024-10-17 13:40:16 +0300

    Improve code readability

--- src/FrameTest.java
+++ src/FrameTest.java
@@ -6,27 +6,19 @@ class FrameTest {
 
     @Test
     void isStrike() {
-        Frame frame1 = new Frame(10);
-        Frame frame2 = new Frame(0, 10);
-
-        assertTrue(frame1.isStrike());
-        assertFalse(frame2.isStrike());
+        assertTrue(new Frame(10).isStrike());
+        assertFalse(new Frame(0, 10).isStrike());
     }
 
     @Test
     void isSpare() {
-        Frame frame = new Frame(3, 7);
-        Frame frame2 = new Frame(3, 6);
-
-        assertTrue(frame.isSpare());
-        assertFalse(frame2.isSpare());
+        assertTrue(new Frame(3, 7).isSpare());
+        assertFalse(new Frame(3, 6).isSpare());
     }
 
     @Test
     void getFrameSum() {
-        Frame frame = new Frame(3, 7);
-
-        assertEquals(10, frame.getFrameSum());
+        assertEquals(10, new Frame(3, 7).getFrameSum());
     }
 
     @Test
